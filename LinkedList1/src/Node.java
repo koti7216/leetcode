851b4391a745ca580fabc028851a71a0500e9b16
Koti <koti7216@gmail.com>
2022-08-15 15:07:28 -0400

    copy random linked list

--- LinkedList1/src/Node.java
+++ LinkedList1/src/Node.java
@@ -3,7 +3,10 @@ public class Node {
     public Node prev;
     public Node next;
     public Node child;
+    public Node random;
 public Node(int val){
     this.val = val;
+    this.next = null;
+    this.random = null;
 }
 }
