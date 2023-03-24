b02d216348ff257da183722b287d6eba536d5cdd
koti <97489851+koti7216@users.noreply.github.com>
2023-03-24 16:05:39 -0400

    Graph (#28)
    * max area
    
    * number of islands
    
    * computerOnOff
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode
    
    * leetcode

---
+++ graph/src/CloneGraph.java
@@ -0,0 +1,61 @@
+import java.util.*;
+//Given a reference of a node in a connected undirected graph.
+//
+//Return a deep copy (clone) of the graph.
+//
+//Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+public class CloneGraph {
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+
+   /* class Solution {
+        HashMap<Node,Node> h = new HashMap<>();
+        public Node cloneGraph(Node node) {
+            if(node == null)
+                return null;
+            if(h.containsKey(node))
+                return h.get(node);
+
+            Node cn = new Node(node.val,new ArrayList<>());
+            h.put(node,cn);
+            for(Node n:node.neighbors){
+                cn.neighbors.add(cloneGraph(n));}
+
+            return cn;
+        }
+    }*/
+   class Solution {
+       Node h[] = new Node[101];
+       public Node cloneGraph(Node node) {
+           if(node == null)
+               return null;
+           if(h[node.val]!=null)
+               return h[node.val];
+
+           Node cn = new Node(node.val,new ArrayList<>());
+           h[cn.val]=cn;
+           for(Node n:node.neighbors){
+               cn.neighbors.add(cloneGraph(n));}
+
+           return cn;
+       }
+   }
+}
